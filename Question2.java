927a9c40af4d530352d9cbd02ac0b7b849b409b1
mbuescher <mbuescher@hb.edu>
2017-11-01 15:09:36 -0400

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,25 @@
+/** A framework for solving Question #2 in the 2016 Baldwin-Wallace
+ * High School coding competition.  
+ * 
+ * Instructions available at <a>https://contest.acm.bw.edu/contest/problems/</a>
+ */
+
+import java.util.Scanner;
+
+public class Question2
+{
+    
+public static void main (String[] args)
+{
+    Scanner keyboard = new Scanner(System.in);
+    int numCases = keyboard.nextInt();
+    
+    for (int n = 1; n <= numCases; n++)
+    {
+        // solve the question!
+    }
+    
+    keyboard.close();
+}
+
+}
